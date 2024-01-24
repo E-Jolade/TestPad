ca64cd75cc22b439bfb1e3451144e91c1f9779fd
Emmanuel Jolade <emmanueljolade@gmail.com>
2024-01-24 11:03:49 -0500

    Created isPalindrome function

--- Testpad.js
+++ Testpad.js
@@ -1,6 +1,6 @@
 /* 1/23 Daily Byte Problem. Reverse String */ 
 
-var reverseString = function(s) {
+let reverseString = function(s) {
     let reverse = [];
     for(let i = s.length; i >= 0; i--){
         reverse.push(s.charAt(i))
@@ -11,3 +11,20 @@ var reverseString = function(s) {
 };
 
 //console.log(reverseString("The Daily Byte"));
+
+
+/* 1/24 Daily Byte Problem. Valid Palindrome */
+
+let isPalindrome = function(s) {
+    const regex = new RegExp(/[\W_]*/g);
+    let palindrome = s.toLowerCase().replace(regex, '');
+
+    for(let i = 0; i < s.length / 2; i++){
+        if(palindrome[i] !== palindrome[palindrome.length - i - 1]){
+            return false;
+        }
+    }
+    return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama."))
